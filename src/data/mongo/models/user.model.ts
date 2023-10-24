759e8472747d8ec5b488a462a4c646306dcab68f
Max Alva <alva85@gmail.com>
2023-10-24 08:57:45 -0500

    user model

---
+++ src/data/mongo/models/user.model.ts
@@ -0,0 +1,27 @@
+import { model, Schema } from 'mongoose';
+
+export const userSchema = new Schema( {
+    name: {
+        type: String,
+        required: [ true, 'Name is required' ],
+    },
+    email: {
+        type: String,
+        required: [ true, 'Email is required' ],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [ true, 'Password is required' ],
+    },
+    img: {
+        type: String,
+    },
+    role: {
+        type: [ String ],
+        enum: [ 'ADMIN_ROLE', 'USER_ROLE' ],
+        default: [ 'USER_ROLE' ],
+    },
+} );
+
+export const UserModel = model( 'User', userSchema );
